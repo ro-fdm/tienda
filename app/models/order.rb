13b0cb43319570652e7c04de9e9edc7c8ad7f3a7
Rocio FDM <rocio.fdez.diaz.meco@gmail.com>
2014-03-30 19:29:31 +0200

    meto mas restricciones en el reject-if

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
 	has_many :line_items
-	has_many :products, through: :line_item
-	accepts_nested_attributes_for :line_items, :allow_destroy => true
+	has_many :products, through: :line_items
+	accepts_nested_attributes_for :line_items, :allow_destroy => true,  reject_if: proc {|atts| atts['quantity'].to_i < 1 || atts['price'].to_i < 1 }
 end
