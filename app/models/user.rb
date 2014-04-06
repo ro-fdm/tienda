13468ca14dbcbcd9b0c9c62d7830ad35f4fe500e
Rocio FDM <rocio.fdez.diaz.meco@gmail.com>
2014-04-06 13:21:50 +0200

    validaciones para la creacion de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	validates :name,  presence: true
+	validates :email, presence: true
+	validates :email, uniqueness: true
+	validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
