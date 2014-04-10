c596318298ad31142b1e5f4e5038bbd0b5786729
Rocio FDM <rocio.fdez.diaz.meco@gmail.com>
2014-04-11 00:25:45 +0200

    cambios en application controller para reconocer una sesion no cerrada

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,17 +10,16 @@ class SessionsController < ApplicationController
   # POST /sessions
   # POST /sessions.json
   def create
-    @session = Session.new(session_params)
 
 		user = User.where("name = ? or email = ?", params[:username], params[:username]  ).first
 		#seria el equivalente a @user= User.where("name = ? or email = ?", eou.name, eou.email)??
 	
 			if user && user.authenticate(params[:password])
-				@session = Session.create(:user_id => user.id)
-       cookies[:user_id] = user.id
+				session[:user_id] = user.id
+       	cookies[:user_id] = user.id
 				redirect_to root_path, :notice => "Logueado"
 			else
-				format.html { render new, :notice  =>'Datos incorrectos' }
+				 render "new"
 			end
 
   end
@@ -28,15 +27,10 @@ class SessionsController < ApplicationController
  
   # DELETE /sessions/1
   # DELETE /sessions/1.json
-  def destroy
-     @session.destroy
+  def destroy		
     cookies[:user_id] = nil
-
-    respond_to do |format|
-     format.html { redirect_to root_url }
-     format.json { head :no_content }
-     end
-   end
+    redirect_to root_url , :notice => "Has salido de tu cuenta"
+  end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
