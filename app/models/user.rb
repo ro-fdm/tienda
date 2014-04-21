84c479ea1c13b8f39aa15b3e5b7812f08957f19e
Rocio FDM <rocio.fdez.diaz.meco@gmail.com>
2014-04-22 00:45:58 +0200

    arreglar validaciones del usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :name, :email, presence: true
 	validates :email, uniqueness: true
-	validates :email, format { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on :create}
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create}
 	validates :password, confirmation: true
-	validates :password, length: {minimum :8}, on: :create
+	validates :password, length: {:minimum => 8}, on: :create
   validates :password_confirmation, presence: true
  # validates :password,  format: {with: \A[a-zA-Z]+\z
   
